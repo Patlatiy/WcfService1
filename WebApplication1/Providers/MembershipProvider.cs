45682fbe38983ddcb9cb2c0168cff3e58e644b14
Snay <me@snay.me>
2014-04-04 15:18:21 +0400

    Providers comments

--- WebApplication1/Providers/MembershipProvider.cs
+++ WebApplication1/Providers/MembershipProvider.cs
@@ -6,10 +6,24 @@ using WebStore.DbWorker;
 
 namespace WebStore.Providers
 {
+    /// <summary>
+    /// This membership provider is a layer between ASP.NET membership mechanisms and entity framework
+    /// </summary>
     public class CustomMembershipProvider : MembershipProvider
     {
         private readonly WebStoreEntities _dbContext = DbContext.Instance;
-
+        /// <summary>
+        /// Create a new user with specified parameters
+        /// </summary>
+        /// <param name="username">User login</param>
+        /// <param name="password">Password</param>
+        /// <param name="email">Email</param>
+        /// <param name="passwordQuestion">(not used)</param>
+        /// <param name="passwordAnswer">(not used)</param>
+        /// <param name="isApproved">(not used)</param>
+        /// <param name="providerUserKey">(not used)</param>
+        /// <param name="status">(not used)</param>
+        /// <returns>Created MembershipUser object</returns>
         public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer,
             bool isApproved, object providerUserKey, out MembershipCreateStatus status)
         {
@@ -59,6 +73,13 @@ namespace WebStore.Providers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Changes a password for a user with specified login
+        /// </summary>
+        /// <param name="username">User login</param>
+        /// <param name="oldPassword">Old password</param>
+        /// <param name="newPassword">New password</param>
+        /// <returns>True if successful, false if not</returns>
         public override bool ChangePassword(string username, string oldPassword, string newPassword)
         {
             var user = _dbContext.Users.First(usr => usr.Login == username);
@@ -73,9 +94,15 @@ namespace WebStore.Providers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Updates a user with new information provided in MembershipUser object
+        /// </summary>
+        /// <param name="user">MembershipUser object, containing updated information</param>
         public override void UpdateUser(MembershipUser user)
         {
-            var dbUser = _dbContext.Users.First(usr => usr.Login == user.UserName);
+            var dbUser = _dbContext.Users.FirstOrDefault(usr => usr.Login == user.UserName);
+            if (dbUser == null) return;
+
             dbUser.Name = user.UserName;
             dbUser.Login = user.Comment;
             dbUser.Password = user.GetPassword();
@@ -85,15 +112,31 @@ namespace WebStore.Providers
             _dbContext.SaveChanges();
         }
 
+        /// <summary>
+        /// Validates if a user with given credentials does exist
+        /// </summary>
+        /// <param name="username">User login</param>
+        /// <param name="password">Password</param>
+        /// <returns>True if credentials are valid, false if they aren't or user is blocked</returns>
         public override bool ValidateUser(string username, string password)
         {
-            var user = DbContext.Instance.Users.FirstOrDefault(usr => usr.Login == username && usr.Password == password);
+            var user = _dbContext.Users.FirstOrDefault(usr => usr.Login == username && usr.Password == password);
             return user != null && !user.IsBlocked;
         }
 
+        /// <summary>
+        /// Unlocks a user with given login
+        /// </summary>
+        /// <param name="userName">User login</param>
+        /// <returns>True if successful, false if not</returns>
         public override bool UnlockUser(string userName)
         {
-            _dbContext.Users.First(usr => usr.Login == userName).IsBlocked = false;
+            var user = _dbContext.Users.FirstOrDefault(usr => usr.Login == userName);
+            if (user == null) 
+                return false;
+
+            user.IsBlocked = false;
+
             _dbContext.SaveChanges();
             return true;
         }
@@ -103,9 +146,17 @@ namespace WebStore.Providers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets a MembershipUser object containing information about user with specified login
+        /// </summary>
+        /// <param name="username">User login</param>
+        /// <param name="userIsOnline">(not used)</param>
+        /// <returns>A MembershipUser object containing information about a user or null if a user doesn't exist</returns>
         public override MembershipUser GetUser(string username, bool userIsOnline)
         {
-            var user = DbContext.Instance.Users.First(usr => usr.Login == username);
+            var user = DbContext.Instance.Users.FirstOrDefault(usr => usr.Login == username);
+            if (user == null)
+                return null;
             return new MembershipUser(
                 providerName: "CustomMembershipProvider",
                 name: user.Login,
@@ -122,14 +173,31 @@ namespace WebStore.Providers
                 lastLockoutDate: DateTime.MinValue);
         }
 
+        /// <summary>
+        /// Gets user login for a user with specified email address
+        /// </summary>
+        /// <param name="email">Email address</param>
+        /// <returns>User login, null if user doesn't exist</returns>
         public override string GetUserNameByEmail(string email)
         {
-            return _dbContext.Users.First(usr => usr.Email == email).Login;
+            var user = _dbContext.Users.FirstOrDefault(usr => usr.Email == email);
+            return user == null ? null : user.Login;
         }
 
+        /// <summary>
+        /// Deletes a user with specified login from database
+        /// </summary>
+        /// <param name="username">User login</param>
+        /// <param name="deleteAllRelatedData">(not used)</param>
+        /// <returns>True if successful, false if not</returns>
         public override bool DeleteUser(string username, bool deleteAllRelatedData)
         {
-            _dbContext.Users.Remove(_dbContext.Users.First(usr => usr.Login == username));
+            var user = _dbContext.Users.FirstOrDefault(usr => usr.Login == username);
+            if (user == null)
+                return false;
+
+            _dbContext.Users.Remove(user);
+
             _dbContext.SaveChanges();
             return true;
         }
@@ -164,6 +232,9 @@ namespace WebStore.Providers
             get { throw new NotImplementedException(); }
         }
 
+        /// <summary>
+        /// Determines whether registration requires setting up secret question and answer
+        /// </summary>
         public override bool RequiresQuestionAndAnswer
         {
             get { return false; }
@@ -171,6 +242,9 @@ namespace WebStore.Providers
 
         public override string ApplicationName { get; set; }
 
+        /// <summary>
+        /// Maximum login attempts with invalid password
+        /// </summary>
         public override int MaxInvalidPasswordAttempts
         {
             get { return 10; }
@@ -181,6 +255,9 @@ namespace WebStore.Providers
             get { throw new NotImplementedException(); }
         }
 
+        /// <summary>
+        /// Determines whether registration requires a unique email from user
+        /// </summary>
         public override bool RequiresUniqueEmail
         {
             get { return true; }
@@ -191,16 +268,25 @@ namespace WebStore.Providers
             get { throw new NotImplementedException(); }
         }
 
+        /// <summary>
+        /// Minimum required password length
+        /// </summary>
         public override int MinRequiredPasswordLength
         {
             get { return 6; }
         }
 
+        /// <summary>
+        /// Minimum required non-alphanumeric characters in password
+        /// </summary>
         public override int MinRequiredNonAlphanumericCharacters
         {
             get { return 0; }
         }
 
+        /// <summary>
+        /// Password strength RegEx
+        /// </summary>
         public override string PasswordStrengthRegularExpression
         {
             get { return @"^.*(?=.{6,})(?=.*\d).*$"; }
--- WebApplication1/Providers/RoleProvider.cs
+++ WebApplication1/Providers/RoleProvider.cs
@@ -5,21 +5,39 @@ using WebStore.DbWorker;
 
 namespace WebStore.Providers
 {
+    /// <summary>
+    /// Temporary enum specifying which IDs roles does have in database
+    /// </summary>
     public enum UserRoles { Admin = 2, Salesperson = 4, User = 5 }
 
+    /// <summary>
+    /// This role provider is a layer between ASP.NET role mechanisms and entity framework
+    /// </summary>
     public class CustomRoleProvider : System.Web.Security.RoleProvider
     {
         private readonly WebStoreEntities _dbContext = DbContext.Instance;
 
+        /// <summary>
+        /// Verifies if user with specified login is in a role with specified name
+        /// </summary>
+        /// <param name="username">User login</param>
+        /// <param name="roleName">Role name</param>
+        /// <returns>True if user is in role, false if user doesn't exist or isn't in role</returns>
         public override bool IsUserInRole(string username, string roleName)
         {
-            var user = _dbContext.Users.First(usr => usr.Login == username);
-            return user.UserRole.Name == roleName;
+            var user = _dbContext.Users.FirstOrDefault(usr => usr.Login == username);
+            return (user != null) && user.UserRole.Name == roleName;
         }
 
+        /// <summary>
+        /// Get roles for user with specified login
+        /// </summary>
+        /// <param name="username">User login</param>
+        /// <returns>Array of role names for user</returns>
         public override string[] GetRolesForUser(string username)
         {
-            return new[] { _dbContext.Users.First(usr => usr.Login == username).UserRole.Name };
+            var user = _dbContext.Users.FirstOrDefault(usr => usr.Login == username);
+            return user == null ? null : new[] { user.UserRole.Name };
         }
 
         public override void CreateRole(string roleName)
@@ -32,6 +50,11 @@ namespace WebStore.Providers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Checks if role exists
+        /// </summary>
+        /// <param name="roleName">Role name</param>
+        /// <returns>True if role exists, false if it doesn't</returns>
         public override bool RoleExists(string roleName)
         {
             return _dbContext.UserRoles.Select(role => role.Name == roleName).Count() != 0;
@@ -42,12 +65,22 @@ namespace WebStore.Providers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Resets roles for users with specified logins
+        /// </summary>
+        /// <param name="usernames">User logins</param>
+        /// <param name="roleNames">(not used)</param>
         public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
         {
+            var userRole = _dbContext.UserRoles.FirstOrDefault(usrRole => usrRole.ID == (byte)UserRoles.User);
             foreach (var username in usernames)
             {
-                var user = _dbContext.Users.First(usr => usr.Login == username);
-                user.UserRole = _dbContext.UserRoles.First(usrRole => usrRole.ID == (byte)UserRoles.User);
+                var user = _dbContext.Users.FirstOrDefault(usr => usr.Login == username);
+                if (user == null)
+                    continue;
+
+                user.UserRole = (userRole == null) ? null : user.UserRole = userRole;
+                
                 user.RoleID = (byte) UserRoles.User;
                 _dbContext.SaveChanges();
             }
@@ -58,6 +91,10 @@ namespace WebStore.Providers
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Retrieves all roles form the database
+        /// </summary>
+        /// <returns>Array of role names</returns>
         public override string[] GetAllRoles()
         {
             return _dbContext.UserRoles.Select(role => role.Name).ToArray();
